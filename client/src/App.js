cebea57e29fbc363a0f1191d3b10cf716a493774
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-31 22:42:58 -0700

    jumbotron and images

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
       <Footer />
     </>
   );
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Jumbotron from "../../components/Jumbotron";
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-9">
+          <Jumbotron />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
