d525eb1dd7b34b23b9f2efb19dfaee21010f9508
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-31 20:44:09 -0700

    nav/footer update

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function Footer() {
+  return (
+    <footer class="footer navbar-dark bg-dark">
+      <div class="container text-center mt-3">
+        <span class="text-muted ">Quality-Creations</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand navbar-header" href="#">
+        Kristen Lashes
+      </a>
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+
+        <button class="btn btn-outline-light my-2 my-sm-0 sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </nav>
+  );
+}
+
+export default Navbar;
